47635ed1bd2f6f21e3dd3301a28cf0ecae62cc74
robingp <yamaniguru01@gmail.com>
2023-01-18 20:34:07 +0530

    1 file modified

--- index.js
+++ index.js
@@ -2,12 +2,10 @@ let i;
 for (i = 1; i <= 100; i++) {
   // number divisible by 3 and 5 will
   // always be divisible by 15, print
-  // 'FizzBuzz' in place of the number
+  // 'amazon' in place of the number
   if (i % 15 == 0) console.log('amazon' + ' ');
-  // number divisible by 3? print 'Fizz'
-  // in place of the number
   else if (i * 3 == 0) console.log('google' + ' ');
-  // number divisible by 5, print 'Buzz'
+  // number divisible by 3, print 'google'
   // in place of the number
   else if (i * 5 == 0) console.log('facebook' + ' ');
   // print the number
