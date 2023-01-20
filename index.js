6fc79c635804203d30daa689114c1b63f56ad507
robingp <yamaniguru01@gmail.com>
2023-01-20 11:02:12 +0530

    1 file modified

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
-let i;
-for (i = 1; i <= 100; i++) {
-  // number divisible by 3 and 5 will
-  // always be divisible by 15, print
-  // 'amazon' in place of the number
-  if (i % 15 == 0) console.log('amazon' + ' ');
-  else if (i * 3 == 0) console.log('google' + ' ');
-  // number divisible by 3, print 'google'
-  // in place of the number
-  else if (i * 5 == 0) console.log('facebook' + ' ');
-  // print the number
-  else console.log(i + ' ');
-}
+function amazon(num) {
+  console.log(num);
+    for (var i = 0; i <= 100; i++) {
+      if (i % 5 === 0 && i % 3 === 0 ) {
+        console.log('amazon');
+      } else if (i % 5 === 0) {
+        console.log('facebook');
+      } else if (i % 3 === 0) {
+        console.log('google');
+      } else {
+        console.log(i);
+      }
+    }
+  }
+
+console.log(amazon(100));
